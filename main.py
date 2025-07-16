43d8a75a77f4a6c9e116285c880963fda9c13dc4
MechamJonathan <jonsmec95@gmail.com>
2025-07-16 16:32:02 -0600

    implemented user input

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
+import sys
 
 def main():
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
 
+    args = sys.argv[1:]
+    if len(args) < 1:
+        print("Error: No prompt entered")
+        sys.exit(1)
+
+    user_prompt = " ".join(args)
+        
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
 
     response = client.models.generate_content(
         model='gemini-2.0-flash-001', 
-        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+        contents=messages)
     
     print(response.text)
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
