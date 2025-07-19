f6b444acaad88837ac64ca59c75da0457472395f
MechamJonathan <jonsmec95@gmail.com>
2025-07-19 04:51:49 -0600

    updated flags

--- main.py
+++ main.py
@@ -10,12 +10,14 @@ def main():
     client = genai.Client(api_key=api_key)
 
     args = sys.argv[1:]
+    flags = sys.argv[2:]
+
     if len(args) < 1:
         print("Error: No prompt entered")
         sys.exit(1)
 
     user_prompt = " ".join(args)
-        
+
     messages = [
         types.Content(role="user", parts=[types.Part(text=user_prompt)]),
     ]
@@ -24,10 +26,15 @@ def main():
         model='gemini-2.0-flash-001', 
         contents=messages)
     
-    print(response.text)
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-    
+    prompt_tokens = response.usage_metadata.prompt_token_count
+    response_tokens = response.usage_metadata.candidates_token_count
+
+    if "--verbose" in flags:
+        print(f"User prompt: {user_prompt}")
+        print(f"Prompt tokens: {prompt_tokens}")
+        print(f"Response tokens: {response_tokens}")
+    else:
+        print(response.text)
 
 if __name__ == "__main__":
     main()
