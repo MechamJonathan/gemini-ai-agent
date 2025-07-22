c425bddaf3679718d08eeb9ec43c86206dde807c
MechamJonathan <jonsmec95@gmail.com>
2025-07-22 09:47:05 -0600

    updated get_files_info.py and tests

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -3,18 +3,26 @@ import os
 
 
 def get_files_info(working_directory, directory="."):
-    full_path = os.path.join(working_directory, directory)
+    absolute_working_directory = os.path.abspath(working_directory)
+    full_path = os.path.abspath(os.path.join(absolute_working_directory, directory))
+    is_inside = os.path.commonpath([absolute_working_directory, full_path]) == absolute_working_directory
+    result = []
 
-    print(f"full path: {full_path}")
-    print(os.path.abspath(directory))
-    print(os.path.abspath(working_directory))
+    if not is_inside:
+        return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+    if not os.path.isdir(full_path):
+        return f'Error: "{directory}" is not a directory'
+    
+    contents = os.listdir(full_path)
 
-    if os.path.isdir(full_path):
-        print("This is a directory!")
-        print(f'Error: Cannot list "{directory}" as it is outside the permitted working directory')
-    elif not os.path.exists(full_path):
-        print(f'Error: "{directory}" is not a directory')
-    else:
-        print("this is working?")
-
+    for item in contents:
+        try:
+            full_item_path = os.path.join(full_path, item)
+            file_size = os.path.getsize(full_item_path)
+            is_dir = os.path.isdir(full_item_path)
+            result.append(f"- {item}: file_size={file_size} bytes, is_dir={is_dir}")
+        except OSError as e:
+            result.append(f"- {item}: error reading file info ({e})")
+    
+    return "\n".join(result)
     
