edb442fa879a5169425935afbad4d29217ead3fc
MechamJonathan <jonsmec95@gmail.com>
2025-07-26 19:51:53 -0600

    test

--- functions/write_file.py
+++ functions/write_file.py
@@ -8,9 +8,9 @@ def write_file(working_directory, file_path, content):
     full_file_path = os.path.abspath(os.path.join(absolute_working_directory, file_path))
     is_inside = os.path.commonpath([absolute_working_directory, full_file_path]) == absolute_working_directory
 
+
     if not is_inside:
         return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
     if not os.path.isfile(full_file_path):
         return f'Error: File not found or is not a regular file: "{file_path}"'
-    
     
