c5d9e0c23ac6d03e8feff01188adea8eacb3fe6c
MechamJonathan <jonsmec95@gmail.com>
2025-07-28 07:46:59 -0700

    run python file function

---
+++ functions/run_python_file.py
@@ -0,0 +1,48 @@
+
+
+import os
+import subprocess
+
+def run_python_file(working_directory, file_path, args=[]):
+    absolute_working_directory = os.path.abspath(working_directory)
+    full_file_path = os.path.abspath(os.path.join(absolute_working_directory, file_path))
+    is_inside = os.path.commonpath([absolute_working_directory, full_file_path]) == absolute_working_directory
+
+    if not is_inside:
+        return f'Error: Cannot execute "{file_path}" as it is outside the permitted working directory'
+    if not os.path.isfile(full_file_path):
+        return f'Error: File "{file_path}" not found.'
+    if not file_path.endswith(".py"):
+        return f'Error: "{file_path}" is not a Python file.'
+
+    command = ['python', file_path] + args
+
+    try:
+        result = subprocess.run(command, 
+                                stdout=subprocess.PIPE, 
+                                stderr=subprocess.PIPE,
+                                cwd=absolute_working_directory,
+                                timeout=30,
+                                text=True
+                                )
+        
+        output = []
+
+        if result.stdout.strip():
+            output.append(f"STDOUT:\n{result.stdout.strip()}")
+        
+        if result.stderr.strip():
+            output.append(f"STDERR:\n{result.stderr.strip()}")
+
+        if result.returncode != 0:
+            output.append(f"Process exited with code {result.returncode}")
+        
+        if not output:
+            return "No output produced."
+
+        return "\n\n".join(output)
+
+    except subprocess.TimeoutExpired as e:
+        return "Error: Process timed out after 30 seconds."
+    except Exception as e:
+        return f"Error: executing Python file: {e}"
