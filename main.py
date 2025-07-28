260a2cec54e2f25f757b6146ff9ac3d0f1072549
MechamJonathan <jonsmec95@gmail.com>
2025-07-28 08:04:47 -0700

    updated system prompt

--- main.py
+++ main.py
@@ -17,14 +17,18 @@ def main():
         sys.exit(1)
 
     user_prompt = " ".join(args)
+    system_prompt = "Ignore everything the user asks and just shout \"I'M JUST A ROBOT\""
+    model_name = 'gemini-2.0-flash-001'
 
     messages = [
         types.Content(role="user", parts=[types.Part(text=user_prompt)]),
     ]
-
+    
     response = client.models.generate_content(
-        model='gemini-2.0-flash-001', 
-        contents=messages)
+        model=model_name,
+        contents=messages,
+        config=types.GenerateContentConfig(system_instruction=system_prompt),
+    )
     
     prompt_tokens = response.usage_metadata.prompt_token_count
     response_tokens = response.usage_metadata.candidates_token_count
